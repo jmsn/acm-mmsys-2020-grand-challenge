ee4bad911ed026b7f8aef0531177b67821808c1e
Jameson Steiner <jameson.steiner@gmail.com>
2021-09-16 15:58:56 +0200

    Change to use global ffmpeg

--- dash-ll-server/run_gen.sh
+++ dash-ll-server/run_gen.sh
@@ -2,4 +2,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-bash ./gen_live_ingest.sh localhost 9001 ./ffmpeg ${1}
+bash ./gen_live_ingest.sh localhost 9001 ffmpeg ${1}
